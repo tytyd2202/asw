5522f73bc790b58ce5a5180dbfaa771dcf51927a
tytyd2202 <42108142+tytyd2202@users.noreply.github.com>
2018-08-05 10:36:02 +0300

    Add files via upload

---
+++ Fbook-master/Facebook/etc/kamvred.py
@@ -0,0 +1,443 @@
+# Author : IndoKarjok
+# do not edit please!
+
+#!/bin/bash
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "[#...................]5%";
+sleep 0.2;
+clear
+echo '[##..................]10%';
+sleep 0.1;
+clear
+echo '[###.................]15%';
+sleep 0.1;
+clear
+echo "[####................]20%";
+sleep 0.1;
+clear
+echo '[#####...............]25%';
+sleep 0.1;
+clear
+echo '[######..............]30%';
+sleep 0.1;
+clear
+echo "[#######.............]35%";
+sleep 0.1;
+clear
+echo '[########............]40%';
+sleep 0.1;
+clear
+echo '[#########...........]45%';
+sleep 0.1;
+clear
+echo "[##########..........]50%";
+sleep 0.1;
+clear
+echo '[###########.........]55%';
+sleep 0.1;
+clear
+echo '[############........]60%';
+sleep 0.1;
+clear
+echo "[#############.......]65%";
+sleep 0.1;
+clear
+echo '[##############......]70%';
+sleep 0.1;
+clear
+echo '[###############.....]75%';
+sleep 0.1;
+clear
+echo "[################....]80%";
+sleep 0.1;
+clear
+echo '[#################...]85%';
+sleep 0.1;
+clear
+echo '[##################..]90%';
+sleep 0.1;
+clear
+echo '[###################.]95%';
+sleep 0.1;
+clear
+echo "[####################]100%";
+sleep 0.3;
+clear
+echo " \033[32m ________________             ";
+echo '         < ACCSESS DANIED >            ';
+echo '          ----------------             ';
+echo '             \   ,__,                  ';
+echo '              \  (oo)____              ';
+echo '                 (__)    )\            ';
+echo '                    ||--|| *           ';
+sleep 0.2;
+clear
+echo " \033[32m ________________             ";
+echo '         < ACCSESS DANIED >            ';
+echo '          ----------------             ';
+echo '             \   ,__,                  ';
+echo '              \  (--)____              ';
+echo '                 (__)    )\            ';
+echo "                    ||--|| *           ";
+sleep 0.2;
+clear
+echo " \033[32m ________________             ";
+echo '         < ACCSESS DANIED >            ';
+echo '          ----------------             ';
+echo '             \   ,__,                  ';
+echo '              \  (oo)____              ';
+echo '                 (__)    )\            ';
+echo '                    ||--|| *           ';
+sleep 0.2;
+clear
+echo " \033[32m ________________             ";
+echo '         < ACCSESS DANIED >            ';
+echo '          ----------------             ';
+echo '             \   ,__,                  ';
+echo '              \  (--)____              ';
+echo '                 (__)    )\            ';
+echo '      Mwoooh        ||--|| *           ';
+sleep 0.3;
+clear
+echo " \033[32m successful installed.        ";
+sleep 0.2;
+clear
+echo ' \033[32m successful installed..        ';
+sleep 0.2;
+clear
+echo " \033[32m successful installed...        ";
+sleep 0.2;
+clear
+
+echo "\033[32m ===================== Welcome ===================";
+echo '         [+] Coded : Tn.Herp                           ';
+echo '         [+] Id Fb : Gwimusa3                          ';
+echo '         [+] IG    : @Rezadkim                         ';
+echo '         [+] WA    : 0895611252563 & 089620134992      ';
+echo '         ------------------ FaceBook Tools ---------------';
+echo '';
+echo 'Nb: Pantad Typiz Dilarang Menggunakan Tools Ini!';
+echo "    Atau akan terbully. Eh ntar malah nangid :v ";
+sleep 0.7;
+clear
+
+import os, time
+import urllib2
+
+def main():
+     os.system('reset')
+def fbbrute():
+     f = open('fbbrute.py','w')
+     link = 'https://gist.githubusercontent.com/Ranginang67/43f02db69746daff343ac7f9a5d51646/raw/c66bcb2390169fbe4666e8cead3c0c1cc3f5f3ed/gistfile1.txt'
+     open = urllib2.urlopen(link)
+     data = open.read()
+     KONTOL = '%s' %data
+     f.write(KONTOL)
+     f.close()
+     os.system('mv fbbrute.py $HOME')
+     print "Done..."
+     print "the fbbrute script is in the home path"
+def fbook():
+     os.system('git clone https://github.com/Ranginang67/Fbook.git && mv Fbook $HOME')
+     print "Done..."
+def mbf():
+     os.system('git clone https://github.com/pirmansx/mbf && mv mbf $HOME')
+     print "Done..."
+def botfacebook():
+     os.system('git clone https://github.com/Senitopeng/BotFbBangDjon.git && mv BotFbBangDjon $HOME')
+     print "Done..."
+def autoreport():
+     os.system('git clone https://github.com/gshofficialgithubindonesia/autoreport-fb && mv autoreport-fb $HOME')
+     print "Done..."
+
+     
+# ASW LOE :v
+
+banner = '''\a
+  .___         .        .  
+  [__  _. _. _ |_  _  _ ;_/
+  |   (_](_.(/,[_)(_)(_)| \
+
+  
+  
+    1 - fbbrute (bruteforce)
+    2 - Fbook (bruteforce)
+    3 - MBF
+    4 - Bot fb By BangDjon
+    5 - auto report FB
+    
+'''
+main()
+print
+print
+print
+print
+print
+print banner
+fb = raw_input('|+| Bissmillah : ')
+if fb == '1':
+      fbbrute()
+elif fb == '2':
+      fbook()
+elif fb == '3':
+      mbf()
+elif fb == '4':
+      botfacebook()
+elif fb == '5':
+      autoreport()
+else:
+   print "\a\n[x] Wrong input..."
+   time.sleep(0.8)
+   os.system('python2 .FB')
